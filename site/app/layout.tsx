f806aab4c6a8bb89e97ef40903ca0428669c2b7c
isaced <isaced@163.com>
2023-05-22 22:57:55 +0800

    Refactor site with Next.js

---
+++ site/app/layout.tsx
@@ -0,0 +1,17 @@
+import "./globals.css";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Top Crystal Web Frameworks",
+  description: "A list of popular github projects related to Crystal web framework (ranked by stars automatically)",
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
--- site/app/page.tsx
+++ site/app/page.tsx
@@ -1,9 +1,6 @@
-import "./App.css";
-
-import useSWR from "swr";
+import Image from "next/image";
 
 const DATA_URL = "https://cdn.jsdelivr.net/gh/isaced/crystal-web-framework-stars/list.json";
-const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 interface Repo {
   name: string;
@@ -15,17 +12,19 @@ interface Repo {
   last_commit_date: string;
 }
 
-function App() {
-  const { data, error, isLoading } = useSWR<Repo[]>(DATA_URL, fetcher);
-  if (error) return <div>failed to load</div>;
-  if (isLoading) return <div>loading...</div>;
+async function fetchData() {
+  return (await fetch(DATA_URL)).json() as Promise<Repo[]>;
+}
+
+export default async function Home() {
+  const data = await fetchData();
 
   return (
-    <>
+    <main className="py-10 px-3 sm:px-8 lg:px-10 xl:px-20">
       <h1 className="text-5xl">Top Crystal Web Frameworks</h1>
       <div className="mt-4">
         A list of popular github projects related to Crystal web framework (ranked by stars automatically) Please update
-        list.txt (via Pull Request)
+        list.txt (via <a href="https://github.com/isaced/crystal-web-framework-stars/pulls">Pull Request</a>)
       </div>
       <div className="relative overflow-x-auto mt-10">
         <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
@@ -55,7 +54,9 @@ function App() {
             {data?.map((repo) => (
               <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
                 <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                  {repo.name}
+                  <a href={repo.html_url} target="__blank">
+                    {repo.name}
+                  </a>
                 </th>
                 <td className="px-6 py-4">{repo.stargazers_count}</td>
                 <td className="px-6 py-4">{repo.forks_count}</td>
@@ -68,20 +69,31 @@ function App() {
         </table>
       </div>
 
-      <div className="mt-10 text-slate-400">
+      <div className="sm:flex justify-between py-4 text-xs space-y-4">
+        <div className="flex text-slate-400">
+          <a
+            href="https://github.com/isaced/crystal-web-framework-stars"
+            className="flex justify-center items-center space-x-1"
+            target="_blank"
+          >
+            <img
+              src="https://avatars.githubusercontent.com/in/15368?s=48&v=4"
+              className="w-4 h-4 rounded-full overflow-hidden"
+            />
+            <span>isaced/crystal-web-framework-stars</span>
+          </a>
+        </div>
+
         <a
-          href="https://github.com/isaced/crystal-web-framework-stars"
-          className="flex justify-center items-center space-x-1"
+          className="pointer-events-none flex gap-2 lg:pointer-events-auto text-gray-500 dark:text-white"
+          href="https://vercel.com"
+          target="_blank"
+          rel="noopener noreferrer"
         >
-          <img
-            src="https://avatars.githubusercontent.com/in/15368?s=48&v=4"
-            className="w-4 h-4 rounded-full overflow-hidden"
-          />
-          <span>isaced/crystal-web-framework-stars</span>
+          Deploy By{" "}
+          <Image src="/vercel.svg" alt="Vercel Logo" className="dark:invert" width={50} height={50} priority />
         </a>
       </div>
-    </>
+    </main>
   );
 }
-
-export default App;
