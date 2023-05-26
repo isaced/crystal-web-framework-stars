168975bc403bbbfff6e5cfda5279844770b97d3d
isaced <isaced@163.com>
2023-05-26 11:33:44 +0800

    Update site style

--- site/app/page.tsx
+++ site/app/page.tsx
@@ -4,7 +4,8 @@ import localizedFormat from "dayjs/plugin/localizedFormat";
 
 dayjs.extend(localizedFormat);
 
-const DATA_URL = "https://raw.githubusercontent.com/isaced/crystal-web-framework-stars/master/list.json";
+const DATA_URL =
+  "https://gh.api.99988866.xyz/https://raw.githubusercontent.com/isaced/crystal-web-framework-stars/master/list.json";
 
 interface Repo {
   name: string;
@@ -16,6 +17,8 @@ interface Repo {
   last_commit_date: string;
 }
 
+const StarColors = ["text-rose-800", "text-rose-700", "text-rose-600", "text-rose-500", "text-rose-400"];
+
 async function fetchData() {
   return (await fetch(DATA_URL)).json() as Promise<Repo[]>;
 }
@@ -25,7 +28,10 @@ export default async function Home() {
 
   return (
     <main className="py-10 px-3 sm:px-8 lg:px-10 xl:px-20">
-      <h1 className="text-5xl">Top Crystal Web Frameworks</h1>
+      <h1 className="text-5xl flex items-center space-x-2">
+        <Image src="/crystal-logo.png" alt={"Crystal Logo"} width={46} height={46} className=" h-fit" />
+        <span>Top Crystal Web Frameworks</span>
+      </h1>
       <div className="mt-4">
         A list of popular github projects related to Crystal web framework (ranked by stars automatically) Please update{" "}
         <a href="https://github.com/isaced/crystal-web-framework-stars/blob/master/list.txt" target="__blank">
@@ -37,47 +43,29 @@ export default async function Home() {
         </a>
         )
       </div>
-      <div className="relative overflow-x-auto mt-10">
-        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-            <tr>
-              <th scope="col" className="px-6 py-3">
-                Project Name
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Stars
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Forks
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Open Issues
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Description
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Last Commit
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {data?.map((repo, idx) => (
-              <tr key={idx} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                  <a href={repo.html_url} target="__blank">
-                    {repo.name}
-                  </a>
-                </th>
-                <td className="px-6 py-4">{repo.stargazers_count}</td>
-                <td className="px-6 py-4">{repo.forks_count}</td>
-                <td className="px-6 py-4">{repo.open_issues_count}</td>
-                <td className="px-6 py-4">{repo.description}</td>
-                <td className="px-6 py-4">{dayjs.default(repo.last_commit_date).format("LL").toString()}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+      <div className="relative overflow-x-auto mt-10 space-y-3">
+        {data?.map((repo, idx) => (
+          <a
+            href={repo.html_url}
+            target="__blank"
+            className="p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 flex items-center space-x-6"
+          >
+            <div className={`w-20 text-2xl font-bold text-center tracking-tight ${StarColors[idx] ?? "text-gray-900"}`}>
+              {repo.stargazers_count}
+            </div>
+            <div>
+              <div className="flex items-center space-x-3">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{repo.name}</h5>
+                <div className="text-gray-500 text-xs space-x-3">
+                  <span>{dayjs.default(repo.last_commit_date).format("LL").toString()}</span>
+                  <span>{repo.forks_count}</span>
+                  <span>{repo.open_issues_count}</span>
+                </div>
+              </div>
+              <p className="font-normal text-gray-700 dark:text-gray-400">{repo.description}</p>
+            </div>
+          </a>
+        ))}
       </div>
 
       <div className="sm:flex justify-between py-4 text-xs space-y-4">
@@ -90,7 +78,7 @@ export default async function Home() {
             <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24">
               <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
             </svg>
-            <span>isaced/crystal-web-framework-stars</span>
+            <span>crystal-web-framework-stars</span>
           </a>
         </div>
 
