09360600494b6d0e10597e7b675cb3140a93a1cd
isaced <isaced@163.com>
2026-01-13 23:36:12 +0800

    Update dependencies in package.json to latest versions

--- site/next.config.ts
+++ site/next.config.ts
@@ -1,5 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
@@ -9,4 +10,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
